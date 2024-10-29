7aad9ca275c2b3d9b873883a7fc34a97630dab65
Tsering Dhondup <tyddhondup88@gmail.com>
2024-10-29 16:01:44 -0400

    getting and sending product list routes working

--- routes/newProduct.js
+++ routes/newProduct.js
@@ -6,7 +6,7 @@ const serpapiKey = process.env.API_KEY;
 router.get("/products", (req, res, nexst) => {
   // GET REQUEST
   axios
-    .get(`${baseAPI}&q=%22${req.body.productName}%22&api_key=${serpapiKey}`)
+    .get(`${baseAPI}&q=%22${req.query.productName}%22&api_key=${serpapiKey}`)
     .then((respond) => {
       res.status(200).json(respond.data.shopping_results);
     })
