b566138a9cd2e3cd7bf071bdf0e783f7f5228f1f
Tsering Dhondup <tyddhondup88@gmail.com>
2024-10-29 14:01:19 -0400

    routes defined

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ const app = express();
 require("dotenv").config();
 // JSON PARSER
 app.use(express.json());
+// PORT
 const port = process.env.PORT || 8000;
 // CORS
 app.use(cors({ origin: 3000 }));
-// start Express server
+
+// ROUTES
+const products = require("./routes/newProduct");
+// app.get("/products", profileRoute); // fresh data from serpapi API
+// app.get("/product/:id", profileRoute); // collected data from local database
+// app.get("/product/carts", profileRoute); // collected data from local database
+// app.get("/product/likes", profileRoute); // collected data from local database
+// app.get("/recommendation"); // fresh data from serpapi API
+
+// app.post("/product/carts", profileRoute); // add to local database
+// app.post("/product/likes", profileRoute); // add to local database
+
+// app.patch("/product/carts/:id", profileRoute); // update local database
+// app.patch("/product/likes/:id", profileRoute); // update local database
+
+// START EXPRESS SERVER
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
