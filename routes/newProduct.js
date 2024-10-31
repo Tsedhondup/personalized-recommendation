993711a16496526d35083dfba5aa39a9478fc218
Tsering Dhondup <tyddhondup88@gmail.com>
2024-10-31 12:20:03 -0400

    setting product preferences into database working

--- routes/newProduct.js
+++ routes/newProduct.js
@@ -2,16 +2,18 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const fs = require("fs");
-const { validatePreferences } = require("../utilities/handlePreferences");
+const validatePreferences = require("../utilities/handlePreferences");
 const baseAPI = process.env.API_URl;
 const serpapiKey = process.env.API_KEY;
 
 router.get("/products", (req, res, next) => {
-  // GET REQUEST
+  // ADDING PREFERENCES TO DATABASE
+  validatePreferences(req.query.productName);
+
+  // GETTING PRODUCT
   axios
     .get(`${baseAPI}&q=%22${req.query.productName}%22&api_key=${serpapiKey}`)
     .then((respond) => {
-      validatePreferences(req.query.productName);
       res.status(200).json(respond.data.shopping_results);
     })
     .catch((error) => {
