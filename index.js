710df3ed096d01f5abb3979b31fbfa132feb4c31
Tsering Dhondup <tyddhondup88@gmail.com>
2024-11-01 01:29:46 -0400

    getting current data working

--- index.js
+++ index.js
@@ -14,11 +14,13 @@ app.use(cors({ origin: 3000 }));
 // ROUTES
 const productsRoute = require("./routes/newProduct");
 const recommendationRoute = require("./routes/recommendationLists");
+const currentDataRoute = require("./routes/currentData");
 app.get("/products", productsRoute); // fresh data from serpapi API
 // app.get("/product/:id", profileRoute); // collected data from local database
 // app.get("/product/carts", profileRoute); // collected data from local database
 // app.get("/product/likes", profileRoute); // collected data from local database
 app.get("/recommendations", recommendationRoute); // fresh data from serpapi API
+app.get("/currentData", currentDataRoute); // fresh data from serpapi API
 
 // app.post("/product/carts", profileRoute); // add to local database
 // app.post("/product/likes", profileRoute); // add to local database
---
+++ routes/currentData.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+
+router.get("/currentData", (req, res, nexst) => {
+  // GET ALL THE PREFERENCE DATA
+  fs.readFile("data/currentProducts.json", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ message: "network error" });
+    }
+    const parsedData = JSON.parse(data); // parsed data
+    // CHECK TOTAL LENGTH OF PREFERENCE DATA
+    if (parsedData.length > 0) {
+      res.status(200).json({ parsedData });
+    } else {
+      res.status(200).json({ message: "no data" });
+    }
+  });
+});
+module.exports = router;
