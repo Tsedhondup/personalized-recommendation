d0e9a69f9753116b4b0dc53bec49985e74ef562c
Tsering Dhondup <tyddhondup88@gmail.com>
2024-10-31 21:59:11 -0400

    function name changed

--- utilities/handleCurrentData.js
+++ utilities/handleCurrentData.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 // CREATE CURRENT PRODUCTS
-const handleCurrentProducts = (data) => {
+const addCurrentProducts = (data) => {
   fs.writeFile("data/currentProducts.json", JSON.stringify(data), (err) => {
     console.log(err);
   });
 };
 // CREATE CURRENT RECOMMENDATION PRODUCTS
-const handleCurrentRecommendations = (data) => {
+const addCurrentRecommendations = (data) => {
   fs.writeFile(
     "data/currentRecommendations.json",
     JSON.stringify(data),
