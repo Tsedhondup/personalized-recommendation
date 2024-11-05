ee478be291fd36a975cd4b7a2e8dde0db77230f6
Tsering Dhondup <tyddhondup88@gmail.com>
2024-11-05 16:35:20 -0500

    gettingNewProduct code refactored

--- routes/getNewProducts.js
+++ routes/getNewProducts.js
@@ -12,51 +12,27 @@ router.get("/getNewProducts", (req, res, next) => {
   // ADDING PREFERENCES TO DATABASE
   validatePreferences(req.query.productName);
 
-  // axios
-  //   .get(
-  //     `${baseAPI}&q=%22${formateProductName(
-  //       req.query.productName
-  //     )}%22&api_key=${serpapiKey}`
-  //   )
-  //   .then((respond) => {
-  //     // CREATE CURRENT PRODUCTS
-  //     const searchProduct = {
-  //       id: req.query.productId,
-  //       name: req.query.productName,
-  //       productLists: respond.data.shopping_results,
-  //     };
+  axios
+    .get(
+      `${baseAPI}&q=%22${formateProductName(
+        req.query.productName
+      )}%22&api_key=${serpapiKey}`
+    )
+    .then((respond) => {
+      // CREATE CURRENT PRODUCTS
+      const searchProduct = {
+        id: req.query.productListsId,
+        name: req.query.productName,
+        productLists: respond.data.shopping_results,
+      };
 
-  //     addCurrentProducts(respond.data.shopping_results);
-  //     res.status(200).json(respond.data.shopping_results);
-  //   })
-  //   .catch((error) => {
-  //     res.status(500).json("Internal Server Error");
-  //   });
-  const searchProduct = {
-    id: req.query.productListsId,
-    name: req.query.productName,
-    productLists: [
-      {
-        productName: "charger for iphone",
-        preferenceScore: 1,
-      },
-      {
-        productName: "bottle",
-        preferenceScore: 1,
-      },
-      {
-        productName: "ramen",
-        preferenceScore: 10,
-      },
-      {
-        productName:
-          "clinique for men maximum hydrator 72-hour auto-replenishing hydrator",
-        preferenceScore: 4,
-      },
-    ],
-  };
+      addCurrentProducts(respond.data.shopping_results);
+      res.status(200).json(respond.data.shopping_results);
+    })
+    .catch((error) => {
+      res.status(500).json("Internal Server Error");
+    });
 
-  addCurrentProducts(searchProduct);
-  res.status(200).json({ Message: "OK" });
+     
 });
 module.exports = router;
