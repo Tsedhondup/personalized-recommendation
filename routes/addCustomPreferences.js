56601fa758bad205393e3669d1c40a62d92e5a03
Tsering Dhondup <tyddhondup88@gmail.com>
2024-11-10 21:13:21 -0500

    adding custom product preference controller fixed

--- routes/addCustomPreferences.js
+++ routes/addCustomPreferences.js
@@ -2,13 +2,21 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 
-router.post("/addCustomPreferences", (req, res, next) => {
-  console.log(req.body);
-  fs.writeFile("data/preferences.json", JSON.stringify(req.body), (err) => {
+const addPreferencesData = (data) => {
+  fs.writeFile("data/preferences.json", JSON.stringify([data]), (err) => {
     if (err) {
       res.status(500).json({ message: "Internal Server Error" });
     }
     res.status(200).json({ Message: "Preferences recieved" });
   });
+};
+
+router.post("/addCustomPreferences", (req, res, next) => {
+  const preferenceData = {
+    userId: req.body.userId,
+    data: [...req.body.customProductTypes],
+  };
+
+  addPreferencesData(preferenceData);
 });
 module.exports = router;
