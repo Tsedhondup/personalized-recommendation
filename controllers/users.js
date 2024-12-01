7f9d237c8df9891455b40ae1bd98627ad5810283
Tsering Dhondup <tyddhondup88@gmail.com>
2024-12-01 13:00:21 -0500

    add custom product preferences controller working

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,24 @@ const knex = require("knex")(require("../knexfile"));
 // const knex = require("knex")("../knexfile"); // promt to installe sqlite3 : investigate later
 const { v4: uuid } = require("uuid");
 
-const addUser = (_req, res) => {
+const addCustomProducts = (req, res, user) => {
+  for (let i = 0; i < req.body.products.length; i++) {
+    knex("products")
+      .insert({
+        name: req.body.products[i],
+        preference_score: 1,
+        user_id: user.user_id,
+      })
+      .then(() => {
+        res.status(200).json(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+};
+
+const addUser = (req, res) => {
   // CREATE USER SESSION ID
   const sessionId = uuid();
   knex("users")
@@ -12,12 +29,12 @@ const addUser = (_req, res) => {
       knex("users")
         .where("user_id", newUser[0])
         .then((data) => {
-          const respondData = data[0];
-          res.status(200).json(respondData);
+          addCustomProducts(req, res, data[0]);
         });
     })
     .catch((error) => {
       console.log(error);
     });
 };
+
 module.exports = { addUser };
