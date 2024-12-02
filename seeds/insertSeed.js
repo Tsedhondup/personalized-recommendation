271cc11da3fe1120012e1f3e17a96ac9e07212bb
Tsering Dhondup <tyddhondup88@gmail.com>
2024-12-01 21:30:56 -0500

    insertSeed file name changed
    logics for inserting current search data created and added to insertSeed file export function

--- seeds/insertSeed.js
+++ seeds/insertSeed.js
@@ -1,16 +1,22 @@
 const usersData = require("../seed-data/users");
 const productsData = require("../seed-data/products");
 const sourcesData = require("../seed-data/sources");
+const currentSearchData = require("../seed-data/current_searches");
 exports.seed = function (knex) {
   return knex("sources")
     .del()
     .then(() => {
-      return knex("products")
+      return knex("current_searches")
         .del()
         .then(() => {
-          return knex("users").del();
+          return knex("products")
+            .del()
+            .then(() => {
+              return knex("users").del();
+            });
         });
     })
+
     .then(() => {
       return knex("users").insert(usersData);
     })
@@ -19,5 +25,8 @@ exports.seed = function (knex) {
     })
     .then(() => {
       return knex("sources").insert(sourcesData);
+    })
+    .then(() => {
+      return knex("current_searches").insert(currentSearchData);
     });
 };
