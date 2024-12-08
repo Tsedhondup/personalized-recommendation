5e20b8d909d91be5a3fa736200f2c2aca61fbe47
Tsering Dhondup <tyddhondup88@gmail.com>
2024-12-08 18:31:28 -0500

    seed data updated

--- seed-data/likedProducts.js
+++ seed-data/likedProducts.js
@@ -3,18 +3,46 @@
  * @returns { Promise<void> }
  */
 module.exports = [
-    {
-      searchOrigin: "hair color",
-      title: "Wella Color Charm Paints Hair Color Accessories Kit",
-      link: "https://www.google.com/shopping/product/17686777603904502009?gl=us",
-      source: "Ulta Beauty",
-      source_logo:
-        "https://serpapi.com/searches/6750b2a849ea5565e9d92d95/images/3cf599fca92ee9d6210e89deb8f2910b5087d38da253b07e6c49689e721d34d8.png",
-      price: "$6.99",
-      rating: 5,
-      reviews: 4,
-      image:
-        "https://serpapi.com/searches/674fdb64861b9de5ebe7a5ab/images/1a5c822d0cb83da5e9dbe72687b25ad92f3bc18e36eff3e050263b5feb6d2c79.webp",
-    },
-  ];
-  
+  {
+    searchOrigin: "hair color",
+    user_id: 2,
+    title: "Wella Color Charm Paints Hair Color Accessories Kit",
+    link: "https://www.google.com/shopping/product/17686777603904502009?gl=us",
+    source: "Ulta Beauty",
+    source_logo:
+      "https://serpapi.com/searches/6750b2a849ea5565e9d92d95/images/3cf599fca92ee9d6210e89deb8f2910b5087d38da253b07e6c49689e721d34d8.png",
+    price: "$6.99",
+    rating: 5,
+    reviews: 4,
+    image:
+      "https://serpapi.com/searches/674fdb64861b9de5ebe7a5ab/images/1a5c822d0cb83da5e9dbe72687b25ad92f3bc18e36eff3e050263b5feb6d2c79.webp",
+  },
+  {
+    searchOrigin: "hair color",
+    user_id: 3,
+    title: "Wella Color Charm Paints Hair Color Accessories Kit",
+    link: "https://www.google.com/shopping/product/17686777603904502009?gl=us",
+    source: "Ulta Beauty",
+    source_logo:
+      "https://serpapi.com/searches/6750b2a849ea5565e9d92d95/images/3cf599fca92ee9d6210e89deb8f2910b5087d38da253b07e6c49689e721d34d8.png",
+    price: "$6.99",
+    rating: 5,
+    reviews: 4,
+    image:
+      "https://serpapi.com/searches/674fdb64861b9de5ebe7a5ab/images/1a5c822d0cb83da5e9dbe72687b25ad92f3bc18e36eff3e050263b5feb6d2c79.webp",
+  },
+  {
+    searchOrigin: "hair color",
+    user_id: 2,
+    title: "Wella Color Charm Paints Hair Color Accessories Kit",
+    link: "https://www.google.com/shopping/product/17686777603904502009?gl=us",
+    source: "Ulta Beauty",
+    source_logo:
+      "https://serpapi.com/searches/6750b2a849ea5565e9d92d95/images/3cf599fca92ee9d6210e89deb8f2910b5087d38da253b07e6c49689e721d34d8.png",
+    price: "$6.99",
+    rating: 5,
+    reviews: 4,
+    image:
+      "https://serpapi.com/searches/674fdb64861b9de5ebe7a5ab/images/1a5c822d0cb83da5e9dbe72687b25ad92f3bc18e36eff3e050263b5feb6d2c79.webp",
+  },
+];
