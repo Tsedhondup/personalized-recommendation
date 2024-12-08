0d2225c54f931a9b1c2dbde02375bb4c8ea951b4
Tsering Dhondup <tyddhondup88@gmail.com>
2024-12-07 20:41:12 -0500

    reviews key name fixed

--- seed-data/likedProducts.js
+++ seed-data/likedProducts.js
@@ -12,7 +12,7 @@ module.exports = [
         "https://serpapi.com/searches/6750b2a849ea5565e9d92d95/images/3cf599fca92ee9d6210e89deb8f2910b5087d38da253b07e6c49689e721d34d8.png",
       price: "$6.99",
       rating: 5,
-      review: 4,
+      reviews: 4,
       image:
         "https://serpapi.com/searches/674fdb64861b9de5ebe7a5ab/images/1a5c822d0cb83da5e9dbe72687b25ad92f3bc18e36eff3e050263b5feb6d2c79.webp",
     },
--- seeds/insertSeed.js
+++ seeds/insertSeed.js
@@ -2,31 +2,52 @@ const usersData = require("../seed-data/users");
 const productsData = require("../seed-data/products");
 const sourcesData = require("../seed-data/sources");
 const currentSearchData = require("../seed-data/current_searches");
-exports.seed = function (knex) {
-  return knex("sources")
-    .del()
-    .then(() => {
-      return knex("current_searches")
-        .del()
-        .then(() => {
-          return knex("products")
-            .del()
-            .then(() => {
-              return knex("users").del();
-            });
-        });
-    })
+const historyData = require("../seed-data/history");
+const savedData = require("../seed-data/saveProducts");
+const likedData = require("../seed-data/likedProducts");
+exports.seed = async function (knex) {
+  // DELETE ALL EXISTING DATA
+  await knex("saved_products").del();
+  await knex("liked_products").del();
+  await knex("history_products").del();
+  await knex("sources").del();
+  await knex("current_searches").del();
+  await knex("products").del();
+  await knex("users").del();
 
-    .then(() => {
-      return knex("users").insert(usersData);
-    })
-    .then(() => {
-      return knex("products").insert(productsData);
-    })
-    .then(() => {
-      return knex("sources").insert(sourcesData);
-    })
-    .then(() => {
-      return knex("current_searches").insert(currentSearchData);
-    });
+  // INSERTING NEW SEED DATA
+  await knex("users").insert(usersData);
+  await knex("products").insert(productsData);
+  await knex("current_searches").insert(currentSearchData);
+  await knex("sources").insert(sourcesData);
+  await knex("history_products").insert(historyData);
+  await knex("liked_products").insert(likedData);
+  await knex("saved_products").insert(savedData);
+
+  // return knex("sources")
+  //   .del()
+  //   .then(() => {
+  //     return knex("current_searches")
+  //       .del()
+  //       .then(() => {
+  //         return knex("products")
+  //           .del()
+  //           .then(() => {
+  //             return knex("users").del();
+  //           });
+  //       });
+  //   })
+
+  //   .then(() => {
+  //     return knex("users").insert(usersData);
+  //   })
+  //   .then(() => {
+  //     return knex("products").insert(productsData);
+  //   })
+  //   .then(() => {
+  //     return knex("sources").insert(sourcesData);
+  //   })
+  //   .then(() => {
+  //     return knex("current_searches").insert(currentSearchData);
+  //   });
 };
