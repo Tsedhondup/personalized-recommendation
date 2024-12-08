994d51290b86ff3ad93471b1d9cb18bd16c01760
Tsering Dhondup <tyddhondup88@gmail.com>
2024-12-07 20:31:30 -0500

    history, saved and liked seed data created

---
+++ seed-data/likedProducts.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+module.exports = [
+    {
+      searchOrigin: "hair color",
+      title: "Wella Color Charm Paints Hair Color Accessories Kit",
+      link: "https://www.google.com/shopping/product/17686777603904502009?gl=us",
+      source: "Ulta Beauty",
+      source_logo:
+        "https://serpapi.com/searches/6750b2a849ea5565e9d92d95/images/3cf599fca92ee9d6210e89deb8f2910b5087d38da253b07e6c49689e721d34d8.png",
+      price: "$6.99",
+      rating: 5,
+      review: 4,
+      image:
+        "https://serpapi.com/searches/674fdb64861b9de5ebe7a5ab/images/1a5c822d0cb83da5e9dbe72687b25ad92f3bc18e36eff3e050263b5feb6d2c79.webp",
+    },
+  ];
+  
