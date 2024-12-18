d41eeebd3adfbbcb19de019687f9c5a57165048c
Tsering Dhondup <tyddhondup88@gmail.com>
2024-12-17 23:43:57 -0500

    type error fixed

--- controllers/currentSearchData.js
+++ controllers/currentSearchData.js
@@ -1,22 +1,25 @@
 const fs = require("fs");
 
 const getCurrentSearchData = (req, res) => {
-  fs.readFile(`data/SearchData${req.query.sessionId}`, (error, data) => {
-    if (error) {
-      res.status(500).json([]); // preventing app from crashin on client siteF
-    }
-    const parsedData = JSON.parse(data);
-    if (parsedData.length > 0) {
-      const currentSearchData = parsedData.filter((item) => {
-        return item.sessionId === req.query.currentSearchId;
-      });
-      if (currentSearchData.length > 0) {
-        res.status(200).json(currentSearchData);
-      } else {
-        res.status(200).json([]);
+  fs.readFile(
+    `data/currentSearchData/${req.query.sessionId}.json`,
+    (error, data) => {
+      if (error) {
+        res.status(500).json([]); // preventing app from crashin on client siteF
+      }
+      const parsedData = JSON.parse(data);
+      if (parsedData.length > 0) {
+        const currentSearchData = parsedData.filter((item) => {
+          return item.searchId === req.query.currentSearchId;
+        });
+        if (currentSearchData.length > 0) {
+          res.status(200).json(currentSearchData);
+        } else {
+          res.status(200).json([]);
+        }
       }
     }
-  });
+  );
 };
 
 module.exports = { getCurrentSearchData };
